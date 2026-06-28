0ec9a101f901b791405bc31dda74021d7b30f10f
VictorDavidLeonCarrasco <vleon1308@gmail.com>
2026-06-27 23:01:42 -0500

    arreglo

--- lib/screens/registro/registro_screen.dart
+++ lib/screens/registro/registro_screen.dart
@@ -1,1 +1,16 @@
+import 'package:flutter/material.dart';
 
+class RegistroScreen extends StatelessWidget {
+  const RegistroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registro'),
+        backgroundColor: const Color(0xFF1565C0),
+      ),
+      body: const Center(child: Text('Pantalla de registro en construcción')),
+    );
+  }
+}
